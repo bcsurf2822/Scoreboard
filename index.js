483f978b02fa9a3ed7e8e8dba1ef9aac45eec3ab
ocneb <111916951+bcsurf2822@users.noreply.github.com>
2024-09-28 08:01:24 -0400

    finished project for scoreboard

--- index.js
+++ index.js
@@ -1,2 +1,41 @@
-let homeScore = document.querySelector("#home");
-let awayScore = document.querySelector("#away");
+let homeScoreText = document.querySelector("#home");
+let awayScoreText = document.querySelector("#away");
+
+let homeScore = 0
+let awayScore = 0
+
+function newGame() {
+  homeScoreText.innerText = 0;
+  awayScoreText.innerText = 0;
+}
+
+
+function singlehm() {
+  homeScore ++
+  homeScoreText.innerText = homeScore
+}
+
+function doublehm() {
+  homeScore += 2
+  homeScoreText.innerText =  homeScore
+  console.log("after2", homeScore)
+}
+
+function triplehm() {
+  homeScore += 3
+  homeScoreText.innerText +=3
+}
+
+function singleaw() {
+  awayScore ++
+awayScoreText.innerText = awayScore
+}
+
+function doubleaw() {
+  awayScore += 2
+  awayScoreText.innerText = awayScore
+}
+function tripleaw() {
+  awayScore += 3
+  awayScoreText.innerText = awayScore
+}
